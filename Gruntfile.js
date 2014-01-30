4b645a05d222b8637352a0b376a76d8b5f4c6878
Josh Larson <jplhomer@gmail.com>
2014-01-30 17:53:54 -0600

    Added buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -291,6 +291,20 @@ module.exports = function (grunt) {
                 'svgmin',
                 'htmlmin'
             ]
+        },
+        buildcontrol: {
+            options: {
+                dir: 'dist',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            pages: {
+                options: {
+                    remote: 'git@github.com:jplhomer/hotindsm.git',
+                    branch: 'gh-pages'
+                }
+            }
         }
     });
 
